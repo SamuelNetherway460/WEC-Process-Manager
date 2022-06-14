3c4e442733f3a34e41d4616b7cd0fefeec8b11db
samuel <samuelnetherwaygit@gmail.com>
2022-06-14 12:07:50 +0100

    Updated application version number to 1.0.1

--- src/WECProcessManager/resources/Constants.Designer.cs
+++ src/WECProcessManager/resources/Constants.Designer.cs
@@ -70,7 +70,7 @@ namespace WECProcessManager.resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 1.0.0.
+        ///   Looks up a localized string similar to 1.0.1.
         /// </summary>
         internal static string VERSION_NUMBER {
             get {
